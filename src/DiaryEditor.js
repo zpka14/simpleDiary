7ed41f170f1ea0fe27ce7c680c295da1581e2c60
zpka14 <zpka14@naver.com>
2022-07-06 01:35:41 +0900

    오늘의 일기 제작 - 1일차

---
+++ src/DiaryEditor.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공!");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>Today's Diary 😊</h2>
+      <div>
+        <input
+          value={state.author}
+          onChange={handleChangeState}
+          name="author"
+          placeholder="작성자"
+          type="text"
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={handleChangeState}
+          name="content"
+          placeholder="일기"
+          type="text"
+        />
+      </div>
+      <div>
+        <span>오늘의 감정점수 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>😡</option>
+          <option value={2}>😒</option>
+          <option value={3}>😐</option>
+          <option value={4}>🙂</option>
+          <option value={5}>🤩</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
